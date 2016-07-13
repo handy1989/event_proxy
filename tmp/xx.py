276b055165345dfe4958eeb01cf4b5cbe55e9145
814040839 <zhangmenghan@stnts.com>
2016-07-13 18:52:03 +0800

    add test file

---
+++ tmp/xx.py
@@ -0,0 +1,14 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 3334))
+s.send('GET http://192.168.3.57/tmp.1M HTTP/1.1\r\n')
+s.send('Host: 192.168.3.57\r\n')
+s.send('\r\n')
+f = open('xx', 'w')
+#print s.recv(10240)
+while True:
+    n = s.recv(102400)
+    print "recv %d bytes" % len(n)
+    f.write(n) 
+    if len(n) <= 0:
+        break
