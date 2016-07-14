54b58fa57fb821ec63a3eb35ce266088c064a71c
handy1989 <menghan1218@gmail.com>
2016-07-14 13:59:56 +0800

    new branch

--- http_parse_help.h
+++
@@ -1,9 +0,0 @@
-#ifndef _HTTP_PARSE_HELP_H_
-#define _HTTP_PARSE_HELP_H_
-
-#include "global.h"
-#include <stdint.h>
-
-int32_t ParseHostPort(BufferContext* buffer_context);
-
-#endif // _HTTP_PARSE_HELP_H_
