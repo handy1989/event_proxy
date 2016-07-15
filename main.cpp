50fce89a13931042168372522a2ff231b4863a0f
814040839 <zhangmenghan@stnts.com>
2016-07-15 17:04:39 +0800

    add signal process

--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 #include "logger.h"
 
 #include <stdio.h>
+#include <signal.h>
+
+void Signal(int sig, void (*signal_handler)(int))
+{
+    struct sigaction act;
+    act.sa_handler = signal_handler;
+    sigaction(sig, &act, NULL);
+}
+
+void SignalHandler(int sig)
+{
+    switch (sig)
+    {
+        case SIGPIPE:
+            printf("received SIGPIPE, ignore\n");
+            break;
+        case SIGTERM:
+        case SIGINT:
+            break;
+
+    }
+}
 
 int main(int argc, char** argv)
 {
+    Signal(SIGPIPE, SignalHandler);
     if (argc != 2)
     {
         printf("usage: %s thread_num\n", argv[0]);
